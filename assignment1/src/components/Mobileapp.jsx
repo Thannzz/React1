8f4f38afbb1ce78c05a93a8b83a87fdfdfdf7920
Thannzz <thanigai128@gmail.com>
2022-05-16 23:20:07 +0530

    second commit

---
+++ assignment1/src/components/Mobileapp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Mob = ()=>{
+
+    return <div>
+        <h1>Mobile Operating system</h1>
+        <li>Android</li>
+        <li>BlackBerry</li>
+        <li>iphone</li>
+        <li>Windows Phone</li>
+        <br />
+        <br />
+        <h1>Mobile Manufacturers</h1>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>MIcromax</li>
+        <li>Apple</li>
+        </div>
+        
+        
+    
+}
+
+export default Mob
